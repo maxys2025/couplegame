679b9cc4bb6557172efca7ea1ff65bbc29ad644e
maxys2025 <melyx2025@gmail.com>
2024-11-10 18:57:58 +0100

    Update game.js

--- game.js
+++ game.js
@@ -5,12 +5,21 @@ let indiceDomanda = 0;
 
 // Carica il file JSON con le domande
 fetch('domande.json')
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Errore nel caricare il file JSON');
+        }
+        return response.json();
+    })
     .then(data => {
         domande = data;
+        console.log('Domande caricate:', domande);  // Aggiungi il log per vedere i dati caricati
         mostraDomanda();  // Mostra la prima domanda appena il JSON è caricato
     })
-    .catch(error => console.error('Errore nel caricare il file JSON:', error));
+    .catch(error => {
+        console.error('Errore nel caricare il file JSON:', error);
+        alert('Impossibile caricare le domande. Controlla la console per ulteriori dettagli.');
+    });
 
 function mostraDomanda() {
     if (indiceDomanda < domande.length) {
